33191cdaec241a4260fc3208c4cff457b7e642e4
ylitka8790 <65488701+ylitka8790@users.noreply.github.com>
2021-05-17 04:13:50 +0700

    Add files via upload

---
+++ untitled1/src/main/java/Main.java
@@ -0,0 +1,11 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Main {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "C:\\tools\\chromedriver\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://google.com");
+    }
+}
